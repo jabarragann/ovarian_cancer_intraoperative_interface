e18b1488bd9d1a63f7fdf7d5f9ddbc0f8ef94464
Juan Antonio Barragan <jbarrag3@jh.edu>
2025-09-11 19:12:14 +0200

    Refractor

--- src/main.py
+++ src/main.py
@@ -1,11 +1,9 @@
-from enum import Enum
 from functools import wraps
-import seaborn as sns
 import time
-from vedo import Volume, show, colors, Mesh, Light
+from vedo import Volume,  colors, Mesh, Light
 from pathlib import Path
 from vedo import Plotter
-from vedo.applications import Slicer2DPlotter
+from QuadrantInformation import QuadrantsInformation
 
 
 def time_init(func):
@@ -18,82 +16,6 @@ def time_init(func):
         return result
 
     return wrapper
-
-
-regions_color_palette = sns.color_palette("Set2", 7).as_hex()
-
-
-class QuadrantsInformation(Enum):
-    CENTRAL_AND_BOWEL = (
-        0,
-        "Central and Bowel Resection (BR)",
-        "central_quadrant",
-        regions_color_palette[0],
-    )
-    LEFT_UPPER_QUADRANT = (
-        1,
-        "Left Upper Quadrant (LUQ)",
-        "left_upper_quadrant",
-        regions_color_palette[1],
-    )
-    UPPER_RIGHT_QUADRANT = (
-        2,
-        "Upper Right Quadrant (URQ)",
-        "upper_right_quadrant",
-        regions_color_palette[2],
-    )
-    LEFT_FLANK_AND_BOWEL = (
-        3,
-        "Left Flank and Bowel Resection (BR)",
-        "left_flank_quadrant",
-        regions_color_palette[3],
-    )
-    RIGHT_FLANK_AND_BOWEL = (
-        4,
-        "Right Flank and Bowel Resection",
-        "right_flank_quadrant",
-        regions_color_palette[4],
-    )
-    SMALL_BOWEL = (5, "Small bowel", "small_bowel_quadrant", regions_color_palette[5])
-    PELVIC_REGION = (
-        6,
-        "Pelvic Region",
-        "pelvic_region_quadrant",
-        regions_color_palette[6],
-    )
-
-    def __init__(self, id: int, mosaic_label: str, short_name: str, color: str):
-        self._value = id
-        self._name = mosaic_label
-        self._short_name = short_name
-        self._color = color
-
-    @classmethod
-    def from_file_name(cls, filename: Path):
-        """
-        Assumes file extension is .seg.nrrd
-        """
-        name_no_suffix = filename.with_suffix("").with_suffix("").name
-        for member in cls:
-            if member.short_name == name_no_suffix:
-                return member
-        raise ValueError(f"Unknown quadrant: {name_no_suffix}")
-
-    @property
-    def id(self) -> int:
-        return self._value
-
-    @property
-    def name(self) -> str:
-        return self._name
-
-    @property
-    def short_name(self) -> str:
-        return self._short_name
-
-    @property
-    def color(self) -> str:
-        return self._color
 
 
 def load_ct_scans_regions(
