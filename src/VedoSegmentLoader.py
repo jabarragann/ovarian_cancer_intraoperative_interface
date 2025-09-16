d7c33a3dc8c84fe6ff45568b1a2b773a4c85a3b7
Juan Antonio Barragan <jbarrag3@jh.edu>
2025-09-16 13:14:11 +0200

    Create slides with annotations with VedoSegmentLoader

--- src/VedoSegmentLoader.py
+++ src/VedoSegmentLoader.py
@@ -1,12 +1,13 @@
 import re
 import nrrd
 import numpy as np
-from vedo import Volume, Mesh
+from vedo import Volume, Mesh, colors
 from pathlib import Path
 
 
 class SegmentationNameNotFoundError(Exception):
     """Raised when a segmentation name is not found in SegmentationWrapper."""
+
     pass
 
 
@@ -16,14 +17,12 @@ class VedoSegmentLoader:
         if not segmentation_path.exists():
             raise FileNotFoundError(f"Segmentation file not found: {segmentation_path}")
 
-        self.data, self.header = nrrd.read(
-            str(segmentation_path)
-        )
+        self.data, self.header = nrrd.read(str(segmentation_path))
 
         self.spacing, self.origin = self.parse_header()
         self.dimension = self.header["dimension"]
 
-        self.volume_dict : dict[str, Volume] = {}
+        self.volume_dict: dict[str, Volume] = {}
 
     def parse_header(self):
         # voxel spacing (x,y,z) in mm
@@ -42,7 +41,9 @@ class VedoSegmentLoader:
     def get_volume_from_id(self, label_name: str) -> Volume:
         if label_name not in self.volume_dict:
             raw_data = self.get_data_from_id(label_name)
-            self.volume_dict[label_name] = Volume(raw_data, spacing=self.spacing, origin=self.origin)
+            self.volume_dict[label_name] = Volume(
+                raw_data, spacing=self.spacing, origin=self.origin
+            )
 
         return self.volume_dict[label_name]
 
@@ -52,20 +53,23 @@ class VedoSegmentLoader:
         You need to use header information to figure out the right data volume.
         """
         if len(self.data.shape) > 3:
-            layer = self.get_layer(label_name)
+            layer = self.get_segment_layer(label_name)
             return self.data[layer]
         elif len(self.data.shape) == 3:
             return self.data
         else:
             raise ValueError("Unsupported data shape")
 
-    def get_layer(self, label_name: str) -> int:
+    def get_segment_layer(self, label_name: str) -> int:
         """For multi-layer nrrd files"""
-        segment_id = self.find_segment_id(label_name)
+        segment_id = self.find_segment_index(label_name)
         segment_layer = self.header[f"Segment{segment_id}_Layer"]
         return int(segment_layer)
 
-    def get_id_from_string(self, key_name: str) -> int:
+    def get_segment_label_value(self, segment_index: int) -> int:
+        return int(self.header[f"Segment{segment_index}_LabelValue"])
+
+    def parse_id_from_header_entry(self, key_name: str) -> int:
         match = re.search(r"Segment(\d+)_(Name|ID)", key_name)
         if match:
             id = int(match.group(1))
@@ -73,7 +77,7 @@ class VedoSegmentLoader:
         else:
             raise ValueError(f"Invalid segment format. Provided key: {key_name}")
 
-    def find_segment_id(self, label_name: str) -> int:
+    def find_segment_index(self, label_name: str) -> int:
         for key, value in self.header.items():
             if not isinstance(value, str):
                 continue
@@ -83,10 +87,42 @@ class VedoSegmentLoader:
                 continue
 
             if value == label_name:
-                segment_id = self.get_id_from_string(key)
+                segment_id = self.parse_id_from_header_entry(key)
                 return segment_id
 
         raise SegmentationNameNotFoundError(
             f"Label '{label_name}' not found in {self.segmentation_path}"
         )
 
+    def get_slice(self, segment_name: str, index: int, plane:str, color:str) -> Mesh:
+        assert plane in ['x','y','z'], "Plane must be 'x', 'y' or 'z'"
+
+        segment_index = self.find_segment_index(segment_name)
+        segment_label_value = self.get_segment_label_value(segment_index)
+        segment_volume = self.get_volume_from_id(segment_name)
+
+        # print(f"segment name: {segment_name} segment index: {segment_index}")
+        # print(f"Segment Label Value: {segment_label_value}")
+
+        if plane == "x":
+            seg_slice = segment_volume.xslice(index)
+        elif plane == "y":
+            seg_slice = segment_volume.yslice(index)
+        elif plane == "z":
+            seg_slice = segment_volume.zslice(index)
+        else:
+            raise ValueError(f"Invalid plane: {plane}")
+
+        # Set color of segment
+        lut = colors.build_lut(
+            [
+                (0, (0, 0, 1), 0.0),  # everything else transparent
+                (segment_label_value, color),   
+            ],
+            vmin=0,
+            vmax=segment_label_value,
+        )
+        seg_slice.cmap(lut)
+        seg_slice.alpha(0.3)
+
+        return seg_slice
--- src/test_scripts/read_4d_layer_volume_2.py
+++ src/test_scripts/read_4d_layer_volume_2.py
@@ -36,7 +36,8 @@ def main():
     camera_params = create_camera_params(ct.center(), ct.bounds())
 
     ## Slices and visualization
-    slice_index = 282  # choose the slice along z-axis
+    slice_index = 282 
+    slice_index = 227 
 
     # CT Slice
     ct_slice = ct.yslice(slice_index)
@@ -47,37 +48,13 @@ def main():
     vmax = L + W / 2
     ct_slice.cmap("gray", vmin=vmin, vmax=vmax)
 
-    # Seg slice
-    label_id = 2 #todo
-    lymph_node_volume = vedo_segment_loader.get_volume_from_id("lymph node")
-    seg_slice = lymph_node_volume.yslice(slice_index)
-    lut = colors.build_lut(
-        [
-            (0, (0, 0, 1), 0.0),  # everything else transparent
-            (label_id, "#9725e8"),  # label 2 → red semi-transparent
-        ],
-        vmin=0,
-        vmax=label_id,
-    )
-    seg_slice.cmap(lut)
-    seg_slice.alpha(0.3)
-
-    label_id = 1 #todo
-    primary_volume = vedo_segment_loader.get_volume_from_id("primary")
-    seg_slice2 = primary_volume.yslice(slice_index)
-    lut = colors.build_lut(
-        [
-            (0, (0, 0, 1), 0.0),  # everything else transparent
-            (label_id, "#45e825"),  # label 1 → green semi-transparent
-        ],
-        vmin=0,
-        vmax=label_id,
-    )
-    seg_slice2.cmap(lut)
-    seg_slice2.alpha(0.3)
+    seg_slice1 = vedo_segment_loader.get_slice("lymph node", slice_index, plane='y', color="#9725e8")
+    seg_slice2 = vedo_segment_loader.get_slice("primary", slice_index, plane='y', color="#45e825")
+    seg_slice3 = vedo_segment_loader.get_slice("carcinosis", slice_index, plane='y', color="#f0e964")
 
     # show slice
-    show(seg_slice, seg_slice2, ct_slice, bg="black", camera=camera_params)
+    show(seg_slice1, seg_slice2, seg_slice3, ct_slice, bg="black", camera=camera_params)
+    # show(seg_slice3, ct_slice, bg="black", camera=camera_params)
 
 if __name__ == "__main__":
     main()
